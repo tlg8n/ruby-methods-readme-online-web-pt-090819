3ec83ee722b9077020a28703a07fb33fd1958afe
John Schmidley <tlg8n@outlook.com>
2019-09-09 22:14:08 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,7 @@
 # Use this space to code along with the readme
 
+def
+say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,7 +13,3 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-
-def greeting
-  puts "hello World"
-end
